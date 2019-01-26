e237f5da3e6da32dcf9f48e3ef945cd5a2661e9d
Vit <alfredfid@gmail.com>
2019-01-26 11:06:03 +0200

    add info about data

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -4,3 +4,11 @@ from datasetLoader import loadDataset
 
 if __name__ == "__main__":
     trainX, trainY, testX, testY, classes = loadDataset()
+    
+    mTrain = len(trainX)
+    mTest = len(testX)
+    imgShape = trainX[0].shape
+    
+    print ("Number of training examples: {0}".format(mTrain))
+    print ("Number of testing examples: {0}".format(mTest))
+    print ("Shape of each image: {0}".format(imgShape))
