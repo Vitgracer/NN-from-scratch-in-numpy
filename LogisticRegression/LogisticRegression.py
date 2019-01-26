db160963bb81591a58246fa9514ab89c6383a1d9
Vit <alfredfid@gmail.com>
2019-01-26 12:06:49 +0200

    implement GD

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -59,6 +59,50 @@ def propagate(w, b, X, Y):
     
     return grads, cost 
 
+def optimizeGD(w, b, X, Y, iterations, lr):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (w * h * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (w * h * 3, number of examples)
+    Y -- true "label" vector (1, number of examples)
+    numIterations -- number of epochs (number of GD steps)
+    lr -- learning rate of the gradient descent update rule
+    
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and 
+             bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, 
+             this will be used to plot the learning curve.
+    """
+    
+    costs = []    
+    for i in range(iterations):
+        grads, cost = propagate(w, b, X, Y)
+        
+        # dw size is the same as w - (number of features, 1)
+        # db is scalar 
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # GD update 
+        w = w - lr * dw
+        b = b - lr * db         
+        
+        costs.append(cost)        
+        print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
 if __name__ == "__main__":
     trainX, trainY, testX, testY, classes = loadDataset()
     
