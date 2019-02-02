675554027c5166f2c7fb73c89c5a91af80d08456
Vit <alfredfid@gmail.com>
2019-02-02 09:54:31 +0200

    added forward pass + backward pass

--- 1HiddenLayerNN/1hNN.py
+++ 1HiddenLayerNN/1hNN.py
@@ -9,6 +9,225 @@ NX = lambda X : X.shape[0]
 # output units number 
 NY = lambda Y : Y.shape[0]
 
+def sigmoid(x):
+    s = 1 / (1 + np.exp(-x))
+    return s
+
+def initParams(nx, nh, ny):
+    """
+    Argument:
+    nx -- size of the input layer
+    nh -- size of the hidden layer
+    ny -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (nh, nx)
+                    b1 -- bias vector of shape (nh, 1)
+                    W2 -- weight matrix of shape (ny, nh)
+                    b2 -- bias vector of shape (ny, 1)
+    """
+    
+    np.random.seed(2) # we set up a seed so that your output matches ours although the initialization is random.
+    
+    W1 = np.random.randn(nh, nx) * 0.01
+    b1 = np.zeros((nh, 1))
+    W2 = np.random.randn(ny, nh) * 0.01
+    b2 = np.zeros((ny, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def forwardPropagation(X, parameters):
+    """
+    Argument:
+    X -- input data of size (n_x, m)
+    parameters -- python dictionary containing your parameters (output of initialization function)
+    
+    Returns:
+    A2 -- The sigmoid output of the second activation
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+    """
+    # Retrieve each parameter from the dictionary "parameters"
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Implement Forward Propagation to calculate A2 (probabilities)
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+def computeCost(A2, Y):
+    """
+    Computes the cross-entropy 
+    
+    Arguments:
+    A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+    
+    Returns:
+    cost -- cross-entropy
+    """
+    
+    m = Y.shape[1] # number of examples
+
+    # Compute the cross-entropy cost
+    logprobs = np.multiply(np.log(A2),Y) + np.multiply((1. - Y), np.log(1. - A2)) 
+    cost = -1. / m * np.sum(logprobs)
+    
+    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect. 
+                                # E.g., turns [[17]] into 17 
+    
+    return cost
+
+def backward_propagation(parameters, cache, X, Y):
+    """
+    Implement the backward propagation using the instructions above.
+    
+    Arguments:
+    parameters -- python dictionary containing our parameters 
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
+    X -- input data of shape (2, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    
+    Returns:
+    grads -- python dictionary containing your gradients with respect to different parameters
+    """
+    m = X.shape[1]
+    
+    # First, retrieve W1 and W2 from the dictionary "parameters".
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+        
+    # Retrieve also A1 and A2 from dictionary "cache".
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    # Backward propagation: calculate dW1, db1, dW2, db2. 
+    dZ2 = A2 - Y
+    dW2 = 1. / m * np.dot(dZ2, A1.T)
+    db2 = 1. / m * np.sum(dZ2, axis = 1, keepdims = True)
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dW1 = 1. / m * np.dot(dZ1, X.T) 
+    db1 = 1. / m * np.sum(dZ1, axis = 1, keepdims = True)
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    """
+    Updates parameters using the gradient descent update rule given above
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters 
+    grads -- python dictionary containing your gradients 
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    """
+    # Retrieve each parameter from the dictionary "parameters"
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Retrieve each gradient from the dictionary "grads"
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+    
+    # Update rule for each parameter
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def nnModel(X, Y, nh, num_iterations = 10000):
+    """
+    Arguments:
+    X -- dataset of shape (2, number of examples)
+    Y -- labels of shape (1, number of examples)
+    nh -- size of the hidden layer
+    num_iterations -- Number of iterations in gradient descent loop
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    
+    np.random.seed(3)
+    nx = NX(X)
+    ny = NY(Y)
+    
+    # Initialize parameters, then retrieve W1, b1, W2, b2. Inputs: "n_x, n_h, n_y". Outputs = "W1, b1, W2, b2, parameters".
+    parameters = initParams(nx, nh, ny)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        A2, cache = forwardPropagation(X, parameters)
+        
+        # Cost function. Inputs: "A2, Y". Outputs: "cost".
+        cost = computeCost(A2, Y)
+ 
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        grads = backward_propagation(parameters, cache, X, Y)
+ 
+        # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
+        parameters = update_parameters(parameters, grads)
+        
+        # Print the cost every 1000 iterations
+        print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
+
+def predict(parameters, X):
+    """
+    Using the learned parameters, predicts a class for each example in X
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters 
+    X -- input data of size (n_x, m)
+    
+    Returns
+    predictions -- vector of predictions of our model (red: 0 / blue: 1)
+    """
+    
+    # Computes probabilities using forward propagation, and classifies to 0/1 using 0.5 as the threshold.
+    A2, cache = forwardPropagation(X, parameters)
+    predictions = (A2 > 0.5).astype(int) 
+    
+    return predictions
 
 def plotDecisionBoundary(model, X, y):
     # Set min and max values and give it some padding
@@ -24,7 +243,7 @@ def plotDecisionBoundary(model, X, y):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.ylabel('x2')
     plt.xlabel('x1')
-    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.scatter(X[0, :], X[1, :], c = y.reshape(Y.shape[1]), cmap=plt.cm.Spectral)
     
 
 if __name__ == "__main__":
@@ -37,5 +256,13 @@ if __name__ == "__main__":
     print ('The shape of X is: ' + str(X.shape))
     print ('The shape of Y is: ' + str(Y.shape))
     print ('I have m = %d training examples!' % (X.shape[1]))
+    print ('Number of hidden units : ' + str(NH))
+    print ('Number of input units: ' + str(NX(X)))
+    print ('Number of output units: ' + str(NY(Y)))
     
+    # launch model 
+    parameters = nnModel(X, Y, nh = 4)
+
+    # Plot the decision boundary
+    plotDecisionBoundary(lambda x: predict(parameters, x.T), X, Y)
     
