0e4b2f0d5974e315616887503212f1e4c8c0f204
Vit <alfredfid@gmail.com>
2019-02-04 22:51:14 +0200

    bug fix

--- LHiddenLayerNN/lhNN.py
+++ LHiddenLayerNN/lhNN.py
@@ -201,7 +201,7 @@ def computeCost(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.    
-    cost = -1./m * np.sum(Y * np.log(AL) + (1 - Y) * np.log(1 - AL))
+    cost = (1./m) * (-np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
     
     cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
     assert(cost.shape == ())
